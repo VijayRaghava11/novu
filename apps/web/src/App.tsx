e43943e56049c2500fae02d17aa43883534a07c7
Samineni Vijay Raghava <115659239+VijayRaghava11@users.noreply.github.com>
2022-10-13 00:01:08 +0530

    Update App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -47,11 +47,6 @@ if (SENTRY_DSN) {
   });
 }
 
-const defaultQueryFn = async ({ queryKey }: { queryKey: string }) => {
-  const response = await api.get(`${queryKey[0]}`);
-
-  return response.data?.data;
-};
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -305,8 +300,6 @@ function SpotLightProvider({ children }) {
 
   return <SpotlightContext.Provider value={{ items, addItem, removeItem }}>{children}</SpotlightContext.Provider>;
 }
-function LightProvider({children}){
-//   code goes here
-}
+
 
 export default Sentry.withProfiler(App);
